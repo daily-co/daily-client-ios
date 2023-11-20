e1f50f5bddde6737a0d964995d38d0813007744b
Filipi Fuchter <filipi87@gmail.com>
2023-11-20 10:28:30 -0300

    Package.swift daily-client-ios-0.12.0

--- Package.swift
+++ Package.swift
@@ -13,12 +13,23 @@ let package = Package(
                 "Daily"
             ]
         ),
+        .library(
+            name: "DailySystemBroadcast",
+            targets: [
+                "DailySystemBroadcast"
+            ]
+        ),
     ],
     targets: [
         .binaryTarget(
             name: "Daily",
-            url: "https://www.daily.co/sdk/daily-client-ios-0.11.1.zip",
-            checksum: "9c2d55f5ceb9a1dae9ce25e3af751f2deeae473fde4cd7dd319c3be660515ba6"
+            url: "https://www.daily.co/sdk/daily-client-ios-0.12.0.zip",
+            checksum: "2135eb9ab83b64e036877aa3b5cc4f3a1697a890883fd079dd2972b17a565a85"
+        ),
+        .binaryTarget(
+            name: "DailySystemBroadcast",
+            url: "https://www.daily.co/sdk/daily-system-broadcast-client-ios-0.12.0.zip",
+            checksum: "1f0e9c6143280d6f27b4ba5d5800237011361d77164f1545cf9853f409108837"
         ),
     ]
 )
